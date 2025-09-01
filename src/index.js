85a28960ca81550155254b8d102e767be2e142ae
Danabishop249 <157060145+Danabishop249@users.noreply.github.com>
2025-09-01 12:45:37 -0400

    Axios, css styling and API integration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 //alert("Hello World");
+function displayCS(response) {
+  console.log("Clifton Strengths thought generated");
+  new Typewriter("#strengths", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generateCS(event) {
   event.preventDefault();
 
@@ -6,12 +16,23 @@ function generateCS(event) {
   //csStrengthsElement.innerHTML =
   //"Here is the Clifton Strenghts though of the day.";
 
-  new Typewriter("#strengths", {
-    strings: ["Here is the Clifton Strenghts though of the day"],
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  //build api url
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apikey = "aeb5f974f5fbo4929b0t4d7036cd803d";
+  let prompt = `User instructions: Generate a Clifton Strengths thought of the day about ${instructionsInput.value}`;
+  let context =
+    "You are an expert on all Clifton Strengths.  Your mission is to generate a thought of the day for a Clifton Strengths word that a person can use to improve their strengths. Make sure to follow User instrutions. Sign the results with SheCodes AI inside a <strong> element";
+  let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+  console.log("generating Clifton Strengths thought");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  //build the call to api (axios)
+
+  axios.get(apiurl).then(displayCS);
+  //display the generated clifton strengths thought of the day
 }
 
 let csFormElement = document.querySelector("#cs-generator-form");
