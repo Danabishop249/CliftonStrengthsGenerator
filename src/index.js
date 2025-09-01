390524f7ba72ad4f7934bdd7d0d06f9bdcc5af56
Danabishop249 <157060145+Danabishop249@users.noreply.github.com>
2025-09-01 11:54:34 -0400

    Javascript updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 //alert("Hello World");
+function generateCS(event) {
+  event.preventDefault();
+
+  //let csStrengthsElement = document.querySelector("#strengths");
+  //csStrengthsElement.innerHTML =
+  //"Here is the Clifton Strenghts though of the day.";
+
+  new Typewriter("#strengths", {
+    strings: ["Here is the Clifton Strenghts though of the day"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let csFormElement = document.querySelector("#cs-generator-form");
+csFormElement.addEventListener("submit", generateCS);
